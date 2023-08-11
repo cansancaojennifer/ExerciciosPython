b3c3257db5e600941f266a09c3c991364d89ed88
cansancaojennifer <cansancaojennifer@gmail.com>
2023-08-11 19:02:59 -0300

    desafio 002

---
+++ aulas/desafio002.py
@@ -0,0 +1,4 @@
+Dia = input('Qual dia você nasceu? ')
+Mes = input('Qual mês você nasceu? ')
+Ano = input('Qual ano você nasceu? ')
+print('Você nasce no dia', Dia, 'de', Mes, 'de', Ano, '. Correto?')
