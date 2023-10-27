3bea26bddbe6e0d672be0e6f504f38aae6685fcf
robotictitans <120498502+cansancaojennifer@users.noreply.github.com>
2023-10-27 11:39:11 -0300

    aulas

---
+++ aulas/aula002.py
@@ -0,0 +1,9 @@
+# n1 = input('Digite um numero: ')
+# n2 = input('Digite outro numero: ')
+# soma = n1+n2
+# print(f'A soma vale {soma}')
+
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+soma = n1 + n2
+print(f'A soma vale {soma}')
---
+++ aulas/desafio004.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite um numero: '))
+n2 = float(input('Digite um numero: '))
+soma = n1 + n2
+print(f'Resultado: {soma}')
