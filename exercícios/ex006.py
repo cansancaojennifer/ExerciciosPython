46293fdd81e2fe74ce714f5623f9f123dbaff99b
robotictitans <120498502+cansancaojennifer@users.noreply.github.com>
2023-11-25 14:00:01 -0300

    desafio 006

---
+++ exercícios/ex006.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um numero: '))
+dobro = n * 2
+triplo = n * 3
+raiz_quadrada = n ** (1/2)
+
+print(f'O dobro de {n} é {dobro}, o triplo de {n} é {triplo}, e sua raiz quadrada é {raiz_quadrada}')
